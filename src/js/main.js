83d1bb44a345d436707a81a864210b13e25ec584
ericgonz93 <ericgonz93@hotmail.com>
2016-04-10 19:55:36 -0500

    Two additional DataSets

--- src/js/main.js
+++ src/js/main.js
@@ -10,16 +10,26 @@ var maxAlive = 0;           //How many that were alive during the mostYears
 
 /* Begin: Data Sets */
 var firstTest = {"name1":[1900,1954],"name2":[1943,1988],"name3":[1922,1976], "name4":[1933,1954],
-"name5":[1940,1941], "name6":[1958,1959], "name7":[1958,1959], "name8":[1958,1959]};
+"name5":[1940,1941], "name6":[1958,1959], "name7":[1958,1959], "name8":[1958,1959], "name9": [1978,1999],
+"name10":[1920,1989], "name11":[1900,1964], "name12":[1933,1998], "name13":[1921,1935], "name14": [1900,1978],
+"name15":[1902,1953], "name16":[1907,1986], "name17":[1915,2000], "name18":[1968,2000], "name19": [1937,1984],
+};
 
+var secondTest = {};
+
+var thirdTest = {"name1":[1900,1901],"name2":[1905,1999],"name3":[1912,1933], "name4":[1903,1975],
+"name5":[1923,1958], "name6":[1912,1967], "name7":[1911,1979], "name8":[1900,2000], "name9": [1933,1958],
+"name10":[1978,1989], "name11":[1917,1989], "name12":[1945,1991], "name13":[1917,1920], "name14": [1900,1948],
+"name15":[1952,1953], "name16":[1978,1999], "name17":[1978,2000], "name18":[1901,2000], "name19": [1939,1945],
+"name20":[1922,1983], "name21":[1912,1984], "name22":[1945,2000], "name23":[1909,1961], "name24": [1919,1986],
+};
 /* End: Data Sets */
 
 var data = [];      //Empty array for chart creation
 
 /* Calculate each year's population by counting how many individuals pass basic if-else (y < [0] && Y >[1].*
  * This however does cause O(100n) time complexity. *
- * Time Complexity: O(101n) = O(n) = Theta(n)*
- * Perhaps eliminate having to check already deceased individuals by deleting from array? */
+ * Time Complexity: O(101n) = O(n) = Theta(n). */
 function mostAlive(json){
 var year = 1900, pop = 0;
 mostYear = [1900];
@@ -27,8 +37,8 @@ maxAlive = 0;
 
 while(year <= 2000){           //100 iterations (1900-2000)
 
-    for(item in json){         //Traverse the JSON object for each person's birth and end years. [Birth,End)
-        if((json[item][0] <= year) && (json[item][1] > year)){  //Check if each person is alive and increment for the year if true.
+    for(item in json){         //Traverse the JSON object for each person's birth and end years. [Birth,End]: both inclusive
+        if((json[item][0] <= year) && (json[item][1] >= year)){  //Check if each person is alive and increment for the year if true.
             pop++;
         }
     }
@@ -51,7 +61,10 @@ while(year <= 2000){           //100 iterations (1900-2000)
 }
 
 /* Begin Program Execution*/
-mostAlive(firstTest);
+//mostAlive(firstTest);
+//mostAlive(secondTest);
+mostAlive(thirdTest);
+
 
 var maxYears = " ";
 
@@ -69,7 +82,7 @@ d3.select("#solution")
 
 /*The following is a modified form of the d3.js chart making tutorial, made into a vertical bar graph.*/
 var maxHeight = 300,
-    barWidth = 22;
+    barWidth = 14;
 
 var chart = d3.select('.chart')
     .attr('width', barWidth * data.length +40)
