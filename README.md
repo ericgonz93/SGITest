37cfc5854f4288eb46b62bcb6d6e1e69fec95791
ericgonz93 <ericgonz93@hotmail.com>
2016-04-14 13:03:00 -0500

    Final Comments and testing.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SGITest1
-Determining most individuals alive in span of 100 years given a list of beginning and end years
+Determining most individuals alive in span of 100 years given a list of beginning and end years.
+
+• Charts/Examples of program output found in the Charts folder.
+• Data Sets can be found at the top of the main.js file.
+• Program is run just by opening the index.html file in a browser.
+• Languages/Libraries Used: Javascript, HTML, CSS. D3.JS library used for charts.
--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,6 @@
 * find the year with the most number of people alive.
 */
 
-var mostYear = [1900];      //Array for the years with the most alive
-var maxAlive = 0;           //How many that were alive during the mostYears
-
 /* Begin: Data Sets */
 var firstTest = {"name1":[1900,1954],"name2":[1943,1988],"name3":[1922,1976], "name4":[1933,1954],
 "name5":[1940,1941], "name6":[1958,1959], "name7":[1958,1959], "name8":[1958,1959], "name9": [1978,1999],
@@ -26,19 +23,32 @@ var thirdTest = {"name1":[1900,1901],"name2":[1905,1999],"name3":[1912,1933], "n
 
 var fourthTest = {"name1":[1900,1978],"name2":[1905,1946],"name3":[1912,1913], "name4":[1903,1925]};
 
+var fifthTest = {"name1":[1900,1901],"name2":[1905,1999],"name3":[1912,1933], "name4":[1903,1975],
+"name5":[1923,1958], "name6":[1912,1967], "name7":[1911,1979], "name8":[1900,2000], "name9": [1933,1958],
+"name10":[1978,1989], "name11":[1917,1989], "name12":[1945,1991], "name13":[1917,1920], "name14": [1900,1948],
+"name15":[1952,1953], "name16":[1978,1999], "name17":[1978,2000], "name18":[1901,2000], "name19": [1939,1944],
+"name20":[1922,1983], "name21":[1912,1984], "name22":[1945,2000], "name23":[1909,1961], "name24": [1919,1986],
+"name25":[1916,1984], "name26":[1901,1919], "name27":[1900,1990], "name28":[1905,1947], "name29": [1900,1975],
+"name30":[1912,1976], "name31":[1900,1962], "name32":[1908,2000], "name33":[1915,1967], "name34": [1925,1975],
+"name35":[1904,1968], "name36":[1917,1988], "name37":[1900,1977], "name38":[1903,1923], "name39": [1905,1998]
+};
+
+var sixthTest = {"name1":[1900,1901],"name2":[1902,1902],"name3":[1912,1913], "name4":[1924,1925], "name5":[1924,1924]};
+
 /* End: Data Sets */
 
-var data = [];      //Empty array for chart creation
+var mostYear = [1900];      //Array for the years with the most alive; can be one or many.
+var maxAlive = 0;           //How many that were alive during the mostYears.
+var data = [];              //Empty array for chart creation.
 
 /* Calculate each year's population by counting how many individuals pass basic if-else (y < [0] && Y >[1].*
- * This however does cause O(100n) time complexity. *
- * Time Complexity: O(101n) = O(n) = Theta(n). */
+ * Other options? Fill an array of years, each index a year holding a population, and search the array for the years with the highest pop. */
 function mostAlive(json){
-var year = 1900, pop = 0;
+var year = 1900, pop = 0;       //Set Defaults.
 mostYear = [1900];
 maxAlive = 0;
 
-while(year <= 2000){           //100 iterations (1900-2000)
+while(year <= 2000){           //101 iterations (1900-2000)
 
     for(item in json){         //Traverse the JSON object for each person's birth and end years. [Birth,End]: both inclusive
         if((json[item][0] <= year) && (json[item][1] >= year)){  //Check if each person is alive and increment for the year if true.
@@ -46,11 +56,11 @@ while(year <= 2000){           //100 iterations (1900-2000)
         }
     }
 
-    if(pop == maxAlive){       //If more than one year has the highest amount of population, add to array.
+    if(pop == maxAlive){        //If more than one year has the highest amount of population, add to array.
         mostYear.push(year);
     }
 
-    if(pop > maxAlive){        //if larger population is found, set new year and maximum population.
+    else if(pop > maxAlive){    //if larger population is found, set new year and maximum population.
         mostYear.length = 0;
         mostYear.push(year);
         maxAlive = pop;
@@ -64,19 +74,20 @@ while(year <= 2000){           //100 iterations (1900-2000)
 }
 
 /* Begin Program Execution*/
+
 //mostAlive(firstTest);
 //mostAlive(secondTest);
 //mostAlive(thirdTest);
-mostAlive(fourthTest);
+//mostAlive(fourthTest);
+mostAlive(fifthTest);
+//mostAlive(sixthTest);
 
 var maxYears = " ";
 
- for(var i = 0; i < mostYear.length; i++){
+ for(var i = 0; i < mostYear.length; i++){      //Prepare values for Solution Filling.
     if(i == (mostYear.length - 1)){maxYears = maxYears + mostYear[i];}
        else{maxYears = maxYears + mostYear[i] + ", ";}
  }
-
-console.log(data.length);
 
 /* Fill in Solution information on the screen for user to see. */
 d3.select("#solution")
@@ -85,43 +96,44 @@ d3.select("#solution")
 
 /*The following is a modified form of the d3.js chart making tutorial, made into a vertical bar graph.*/
 var maxHeight = 300,
-    barWidth = 15;
+    barWidth = 13;
 
-var chart = d3.select('.chart')
+var chart = d3.select('.chart')         //Create Chart Space.
     .attr('width', barWidth * data.length +20)
     .attr('height',maxHeight + 30);
 
 var x = d3.scale.linear().domain([1900,2001]).range([0, barWidth * data.length]);
 
-var xAxis = d3.svg.axis().scale(x).orient('bottom').ticks(data.length).tickFormat(d3.format("d"));
+/* Y Axis not set at this time, I feel like it's easier to tell values with numbers on top */
+var xAxis = d3.svg.axis().scale(x).orient('bottom').ticks(data.length).tickFormat(d3.format("d"));  //Scaling for the X Axis and setting tick format
 //var yAxis = d3.svg.axis().scale(y).orient('left').ticks(maxAlive);
 
-var y = d3.scale.linear()       //Setting up a linear scaling for the bars
+var y = d3.scale.linear()       //Setting up a linear scaling for the bars vertically
     .domain([0, d3.max(data)])
-    .range([0, maxHeight-15]);
+    .range([0, maxHeight-15]);  //Subtract from height to allow numbers on top of bars
 
-var bar = chart.selectAll("g")
+var bar = chart.selectAll("g")  //Bar values from data.
     .data(data)
-  .enter().append("g")
+    .enter().append("g")
     .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
 
-chart.append("g")
+chart.append("g")           //Create X Axis and call it.
      .attr('class', 'x axis')
      .attr("transform", "translate(0," + (maxHeight) + ")")
      .call(xAxis)
-     .selectAll('text')
+     .selectAll('text')     //Set tick text to sideways for easier reading.
      .attr("x", "-9px")
      .attr("dx","-.95em")
      .attr("transform", "rotate(-90)" );
 
-bar.append("rect")
+bar.append("rect")          //Actual bars SVGs
     .attr("width", barWidth - 1)
     .attr("x", barWidth/2 )
     .attr("height", y)
     .attr("y", function(d){ return maxHeight - y(d);})
-    .style("fill", function(d){return d == maxAlive ? "gold": "teal";});       //Fill gold in for top years.
+    .style("fill", function(d){return d == maxAlive ? "gold": "teal";});       //Fill gold for top years.
 
-bar.append("text")
+bar.append("text")          //Numbers above bars
     .style("fill", "black")
     .attr("x", barWidth)
     .attr("y", function(d) { return maxHeight - y(d) - 3; })
